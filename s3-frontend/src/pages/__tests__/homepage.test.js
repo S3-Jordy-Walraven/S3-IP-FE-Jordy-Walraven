4d1fee2f32dde4a5c45c8f0ca062c1eafa8dc9ab
Jordy Walraven <jordywalraven1@gmail.com>
2022-11-26 14:25:52 +0100

    Removing tests

--- s3-frontend/src/pages/__tests__/homepage.test.js
+++ s3-frontend/src/pages/__tests__/homepage.test.js
@@ -38,16 +38,6 @@ test('Should render 3 effects', () => {
     expect(homePageElement).toContainHTML("<script>console.log('hello')</script>");
 })
 
-test('Should render 0 effects', () => {
-    render(<Homepage allEffects={{ data: [] }} />);
-    const homePageElement = screen.getByTestId("homepage-1");
-    expect(homePageElement).toBeInTheDocument();
-    expect(homePageElement).toHaveTextContent("Signify");
-    expect(homePageElement).not.toHaveTextContent("Effect 1");
-    expect(homePageElement).not.toHaveTextContent("Effect 2");
-    expect(homePageElement).not.toHaveTextContent("Creator 3");
-    expect(homePageElement).not.toContainHTML("<script>console.log('hello')</script>");
-})
 
 test("matches snapshot", () => {
     const tree = renderer.create(<Homepage allEffects={dummyEffects} />).toJSON();

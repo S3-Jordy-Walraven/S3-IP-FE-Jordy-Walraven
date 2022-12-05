73c907c1e818efc9e93bdd7bc6ea324393a367c0
Jordy Walraven <jordywalraven1@gmail.com>
2022-12-05 13:19:48 +0100

    Adding error handling

--- s3-frontend/src/services/EffectService.js
+++ s3-frontend/src/services/EffectService.js
@@ -3,15 +3,18 @@ import axios from "axios";
 const ACCOUNT_BASE_REST_API_URL = "http://localhost:8080/api/effects";
 class EffectService {
   createEffect(effect) {
-    return axios.post(ACCOUNT_BASE_REST_API_URL, effect);
+    return axios.post(ACCOUNT_BASE_REST_API_URL, effect).catch((error) => {
+    });
   }
 
   async getEffectById(effectId) {
-    return await axios.get(ACCOUNT_BASE_REST_API_URL + "/" + effectId);
+    return await axios.get(ACCOUNT_BASE_REST_API_URL + "/" + effectId).catch((error) => {
+    });
   }
 
   async getAllEffects() {
-    return await axios.get(ACCOUNT_BASE_REST_API_URL);
+    return await axios.get(ACCOUNT_BASE_REST_API_URL).catch((error) => {
+    });
   }
 }
 
